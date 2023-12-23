a85718b0c00d76c3b5ba57d24b63655cec97cd9c
catarinab <catarina.c.bento@tecnico.ulisboa.pt>
2023-12-23 15:26:38 +0000

    day 15 :))

--- day15.py
+++ day15.py
@@ -1,9 +1,3 @@
-f = open("/home/cat/uni/aoc23/inputs/day15.txt", "r")
-lines = f.readlines()
-f.close()
-
-steps = []
-
 def algorithm(step):
     val = 0
     for char in step:
@@ -12,10 +6,43 @@ def algorithm(step):
         val = val % 256
     return val
 
+def addLens(lens, index):
+    lens = lens.split('=')
+    for item in boxes[index]:
+        if item[0] == lens[0]:
+            item[1] = int(lens[1])
+            return
+    boxes[index].append(lens)
+
+def removeLens(lens, index):
+    lens = lens[0:-1]
+    for item in boxes[index]:
+        if item[0] == lens:
+            boxes[index].remove(item)
+            return
+
+f = open("inputs/day15.txt", "r")
+lines = f.readlines()
+f.close()
+
+boxes = []
+for i in range(256):
+    boxes.append([])
 
 val = 0
 line = lines[0].split(',')
 for el in line:
-    val += algorithm(el)
+    limit = -1 if el[-1] == '-' else -2
+    index = algorithm(el[0:limit])
+    print(el, index)
+    if(el[-1] == '-'):
+        removeLens(el, index)
+    else:
+        addLens(el, index)
 
-print(val)
+sum = 0
+for boxIdx in range(len(boxes)):
+    for lensIdx in range(len(boxes[boxIdx])):
+        sum += (boxIdx + 1) * (lensIdx + 1) * int(boxes[boxIdx][lensIdx][1])
+
+print(sum)
