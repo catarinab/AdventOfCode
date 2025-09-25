65926036956f02d6e9a57a6fc24a6064cabaaa7b
catarinab <catarinabento7828@gmail.com>
2025-09-25 12:34:18 +0100

    Unfinished day6

--- 2024/day6.py
+++ 2024/day6.py
@@ -4,6 +4,8 @@ right = (0, 1)
 down = (1, 0)
 left = (0, -1)
 
+directions = [up, right, down, left]
+
 
 #always turn right
 def getNewDirection(currentDirection):
@@ -15,6 +17,7 @@ def getNewDirection(currentDirection):
         return left
     elif currentDirection == left:
         return up
+       
 
 def part1(matrix, currentPosition):
     currDirection = up
@@ -29,10 +32,33 @@ def part1(matrix, currentPosition):
             visitedPositions.append(currentPosition)
         currentPosition = (currentPosition[0] + currDirection[0], currentPosition[1] + currDirection[1])
     return len(visitedPositions)
+    
+def part2(matrix, currentPosition):
+    currDirection = up
+    visitedPositions = []
+    visitedBlocks = []
+    while (currentPosition[0] >= 0 and currentPosition[0] < len(matrix) 
+           and currentPosition[1] >= 0 and currentPosition[1] < len(matrix[0])):
 
+        if matrix[currentPosition[0]][currentPosition[1]] == '#':
+            currentPosition = (currentPosition[0] - currDirection[0], currentPosition[1] - currDirection[1])
+            currDirection = getNewDirection(currDirection)
+        elif currentPosition not in visitedPositions:
+            visitedPositions.append(currentPosition)
+        currentPosition = (currentPosition[0] + currDirection[0], currentPosition[1] + currDirection[1])
+        newDirection = getNewDirection(currDirection)
+        print(currentPosition, newDirection)
+        newPosition = (currentPosition[0] + newDirection[0], currentPosition[1] + newDirection[1])
+        if(newPosition in visitedPositions):
+            print(newPosition)
+            block = (currentPosition[0] + currDirection[0], currentPosition[1] + currDirection[1])
+            if block not in visitedBlocks:
+                visitedBlocks.append(block)
+                print(visitedBlocks)
+    return len(visitedPositions)
 
 def main():
-    input_file = open("input/day6.txt", "r")
+    input_file = open("input/example.txt", "r")
     inputStrings = input_file.readlines()
     input_file.close()
     matrix = []
@@ -41,8 +67,8 @@ def main():
         matrix.append(line)
         if '^' in line:
             startingPoint = (matrix.index(line), line.index('^'))
-
-    print(part1(matrix, startingPoint))
+    #print(part1(matrix, startingPoint))
+    print(part2(matrix, startingPoint))
     
 
 if __name__ == "__main__":
