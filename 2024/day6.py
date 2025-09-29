4f1de0af13a0e455f303ac4520e81e2cd9781f6c
catarinab <catarinabento7828@gmail.com>
2025-09-29 17:12:21 +0100

    day6 part2 progress

--- 2024/day6.py
+++ 2024/day6.py
@@ -4,13 +4,16 @@ right = (0, 1)
 down = (1, 0)
 left = (0, -1)
 
+matrix = []
+visitedDirections = dict()
+visitedPositions = []
+
 directions = [up, right, down, left]
 
 def getRelevantCoordinate(direction):
     if(direction == up or direction == down):
         return 0
     return 1
-
 
 #always turn right
 def getNewDirection(currentDirection):
@@ -27,63 +30,71 @@ def validPosition(position, matrixSize):
     return position[0] >= 0 and position[0] < matrixSize and position[1] >= 0 and position[1] < matrixSize
        
 
-def part1(matrix, currentPosition, direction = up, targetPosition=False):
-    visitedPositions = []
-    while (currentPosition[0] >= 0 and currentPosition[0] < len(matrix) and currentPosition[1] >= 0 and currentPosition[1] < len(matrix[0])):
-        if currentPosition == targetPosition and visitedPositions != []:
-            return -1
-
+def part1(currentPosition, direction = up):
+    matrixSize = len(matrix)
+    while (validPosition(currentPosition, matrixSize)):  
+        currentPosition = (currentPosition[0] + direction[0], currentPosition[1] + direction[1])
+        if not validPosition(currentPosition, matrixSize):
+            break
         if matrix[currentPosition[0]][currentPosition[1]] == '#':
             currentPosition = (currentPosition[0] - direction[0], currentPosition[1] - direction[1])
             direction = getNewDirection(direction)
-        elif currentPosition not in visitedPositions:
-            visitedPositions.append(currentPosition)
-        currentPosition = (currentPosition[0] + direction[0], currentPosition[1] + direction[1])
-               
+        else:
+            if currentPosition not in visitedPositions:
+                visitedPositions.append(currentPosition)
+            if(currentPosition not in visitedDirections):
+                visitedDirections[currentPosition] = direction      
+    print(visitedPositions)
     return len(visitedPositions)
     
-def part2(matrix, currentPosition):
-    currDirection = up
-    visitedPositions = []
-    visitedBlocks = []
-    matrixSize = len(matrix)
-    search = False
-    while (validPosition(currentPosition, matrixSize)):
+    
 
+def findLoop(currentPosition, direction, targetPosition):
+    matrixSize = len(matrix)
+    while (currentPosition[0] >= 0 and currentPosition[0] < matrixSize and currentPosition[1] >= 0 and currentPosition[1] < matrixSize):
         if matrix[currentPosition[0]][currentPosition[1]] == '#':
-            currentPosition = (currentPosition[0] - currDirection[0], currentPosition[1] - currDirection[1])
-            currDirection = getNewDirection(currDirection)
-        elif currentPosition not in visitedPositions:
-            visitedPositions.append(currentPosition)
-        currentPosition = (currentPosition[0] + currDirection[0], currentPosition[1] + currDirection[1])
-        newDirection = getNewDirection(currDirection)
-        block = (currentPosition[0] + currDirection[0], currentPosition[1] + currDirection[1])
-        relevantCoordinate = getRelevantCoordinate(newDirection)
-        for positions in visitedPositions:
-            if positions[relevantCoordinate] != block[relevantCoordinate]:
-                search = True
-                break
-        if search:
-            search = False
-            if  (validPosition(block, matrixSize)) and (matrix[block[0]][block[1]] != '#') and (block not in visitedBlocks) and (block not in visitedPositions) and validPosition(currentPosition, matrixSize) and (matrix[currentPosition[0]][currentPosition[1]] != '#'):
-                matrix[block[0]][block[1]] = '#'
-                if part1(matrix, currentPosition, newDirection, currentPosition) == -1:
-                    visitedBlocks.append(block)
-                matrix[block[0]][block[1]] = '.'
+            currentPosition = (currentPosition[0] - direction[0], currentPosition[1] - direction[1])
+            direction = getNewDirection(direction)
+        currentPosition = (currentPosition[0] + direction[0], currentPosition[1] + direction[1])
+        if currentPosition == targetPosition:
+            return True
+    return False
+
+def part2(startingPoint):
+    currDirection = up
+    visitedBlocks = []
+    search = False
+    matrixSize = len(matrix)
+    
+    print(visitedDirections)
+    
+    for block in visitedDirections:
+        if(block == startingPoint):
+            continue
+        direction = visitedDirections[block]
+        newPosition = (block[0] - direction[0], block[1] - direction[1])
+        newDirection = getNewDirection(direction)
+        print("initializing block: ", block)
+        loop = findLoop(newPosition, newDirection, newPosition)
+        print("finished block: ", block)
+        if(loop):
+            visitedBlocks.append(block)
+        
+    print(visitedBlocks)
     return len(visitedBlocks)
+      
 
 def main():
-    input_file = open("input/input.txt", "r")
+    input_file = open("input/sexample.txt", "r")
     inputStrings = input_file.readlines()
     input_file.close()
-    matrix = []
     for string in inputStrings:
         line = list(string.strip())
         matrix.append(line)
         if '^' in line:
             startingPoint = (matrix.index(line), line.index('^'))
-    print(part1(matrix, startingPoint))
-    print(part2(matrix, startingPoint))
+    print(part1(startingPoint))
+    print(part2(startingPoint))
     
 
 if __name__ == "__main__":
